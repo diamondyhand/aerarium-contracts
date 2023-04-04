2c42592456c80b5fbe08045dfc8266d5e06c2c35
The Chimpy Tuts <124721139+chimpytuts@users.noreply.github.com>
2023-04-04 16:12:04 +0100

    Update FractalMigrator.sol

--- contracts/Migrator/FractalMigrator.sol
+++ contracts/Migrator/FractalMigrator.sol
@@ -1,29 +1,12 @@
-interface IERC721Enumerable is IERC721 {
-
-    /**
-     * @dev Returns the total amount of tokens stored by the contract.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
-     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
-     */
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
-
-    /**
-     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
-     * Use along with {totalSupply} to enumerate all tokens.
-     */
-    function tokenByIndex(uint256 index) external view returns (uint256);
-}
-
 pragma solidity ^0.8.0;
 
 /**
  * @dev Interface of the ERC721 standard as defined in the EIP.
  */
 interface IERC721 {
+
+    function approve(address spender, uint256 tokenId) external returns (bool);
+
     /**
      * @dev Returns the number of tokens in existence.
      */
@@ -44,6 +27,7 @@ interface IERC721 {
      */
     function ownerOf(uint256 tokenId) external view returns (address);
 
+    function balanceOf(address account) external view returns (uint256);
     /**
      * @dev Transfers `tokenId` token from `from` to `to`.
      *
@@ -54,18 +38,36 @@ interface IERC721 {
     function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
+interface IERC721Enumerable is IERC721 {
+
+    /**
+     * @dev Returns the total amount of tokens stored by the contract.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
+     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
+     */
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
+
+    /**
+     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
+     * Use along with {totalSupply} to enumerate all tokens.
+     */
+    function tokenByIndex(uint256 index) external view returns (uint256);
+}
+
 contract TokenSwap {
     IERC721 public oldToken;
     IERC721 public newToken;
     address public owner;
-    address public receiver;
     uint256 _tokenIdGeneral;
 
-    constructor(address  _oldTokenAddress, address _newTokenAddress, address _receiver) {
+    constructor(address  _oldTokenAddress, address _newTokenAddress) {
         oldToken = IERC721(_oldTokenAddress);
         newToken = IERC721(_newTokenAddress);
         owner = msg.sender;
-        receiver = _receiver;
         _tokenIdGeneral = 0;
     }
 
@@ -73,44 +75,38 @@ contract TokenSwap {
         return oldToken.ownerOf(_tokenId) == msg.sender;
     }
 
-    function approveAllOldTokens(uint256 numTokens) public {
-        for (uint256 i = 0; i < numTokens; i++) {
-            uint256 oldTokenId = tokenOfOwnerByIndex(address(msg.sender),i);
-            oldToken.approve(address(this), oldTokenId);
-            }
-        }
-
-    function swap(uint256 numOldTokens) public {
-        require(numOldTokens > 0, "Must send at least one old token");
-        require(numOldTokens == oldToken.balanceOf(msg.sender), "Incorrect number of old tokens sent");
+    function swap(uint256[] memory _tokenIds) public {
     
         // Transfer old tokens to contract
-        for (uint256 i = 0; i < numOldTokens; i++) {
-            uint256 oldTokenId = tokenOfOwnerByIndex(address(msg.sender),i);
-            require(oldToken.ownerOf(oldTokenId) == msg.sender, "Not the owner of old token");
-            oldToken.transferFrom(msg.sender, address(this), oldTokenId);
+        for (uint i = 0; i < _tokenIds.length; i++) {
+            oldToken.transferFrom(msg.sender, address(this), _tokenIds[i]);
         }
     
-    // Transfer new tokens to user with random tokenId from user's tokens
         uint256 numNewTokens = newToken.balanceOf(address(this));
-        require(numNewTokens >= numOldTokens, "Not enough new tokens in contract");
-        for (uint256 i = 0; i < numOldTokens; i++) {
-            uint256 randomTokenIndex = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i))) % numNewTokens;
-            uint256 newTokenId = newToken.tokenOfOwnerByIndex(address(this), randomTokenIndex);
-            newToken.safeTransferFrom(address(this), msg.sender, newTokenId);
-            numNewTokens--;
+        require(numNewTokens >= _tokenIds.length, "Not enough new tokens in contract");
+        for (uint256 i = 0; i < _tokenIds.length; i++) {
+            uint256 newTokenId = IERC721Enumerable(address(newToken)).tokenOfOwnerByIndex(address(this), i);
+            newToken.transferFrom(address(this), msg.sender, newTokenId);
         }
     }
 
     function withdrawOldTokens(uint256[] memory _tokenIds) public {
-        require(msg.sender == owner, "Only the contract owner can withdraw new tokens.");
+        require(msg.sender == owner, "Only the contract owner can withdraw old tokens.");
         for (uint i = 0; i < _tokenIds.length; i++) {
             oldToken.transferFrom(address(this), msg.sender, _tokenIds[i]);
         }
     }
 
+    function withdrawNewTokens(uint256[] memory _tokenIds) public {
+        require(msg.sender == owner, "Only the contract owner can withdraw old tokens.");
+        for (uint i = 0; i < _tokenIds.length; i++) {
+            newToken.transferFrom(address(this), msg.sender, _tokenIds[i]);
+        }
+    }
+
+
     function depositNewTokens(uint256[] memory _tokenIds) public {
-        require(msg.sender == owner, "Only the contract owner can deposit old tokens.");
+        require(msg.sender == owner, "Only the contract owner can deposit new tokens.");
         for (uint i = 0; i < _tokenIds.length; i++) {
             newToken.transferFrom(msg.sender, address(this), _tokenIds[i]);
         }
