02ea945bcac67afeb17f60fa43fd2b16ac2af60e
diamondyhand <masterdevv278@gmail.com>
2023-05-28 11:15:30 -0400

    fix: updated swap function of FractalMigrator regarding AF-117.

--- contracts/Migrator/FractalMigrator.sol
+++ contracts/Migrator/FractalMigrator.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
  * @dev Interface of the ERC721 standard as defined in the EIP.
  */
 interface IERC721 {
-
     function approve(address spender, uint256 tokenId) external returns (bool);
 
     /**
@@ -28,6 +27,7 @@ interface IERC721 {
     function ownerOf(uint256 tokenId) external view returns (address);
 
     function balanceOf(address account) external view returns (uint256);
+
     /**
      * @dev Transfers `tokenId` token from `from` to `to`.
      *
@@ -39,7 +39,6 @@ interface IERC721 {
 }
 
 interface IERC721Enumerable is IERC721 {
-
     /**
      * @dev Returns the total amount of tokens stored by the contract.
      */
@@ -49,7 +48,10 @@ interface IERC721Enumerable is IERC721 {
      * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
      * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
      */
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
+    function tokenOfOwnerByIndex(
+        address owner,
+        uint256 index
+    ) external view returns (uint256 tokenId);
 
     /**
      * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
@@ -64,49 +66,60 @@ contract TokenSwap {
     address public owner;
     uint256 _tokenIdGeneral;
 
-    constructor(address  _oldTokenAddress, address _newTokenAddress) {
+    constructor(address _oldTokenAddress, address _newTokenAddress) {
         oldToken = IERC721(_oldTokenAddress);
         newToken = IERC721(_newTokenAddress);
         owner = msg.sender;
         _tokenIdGeneral = 0;
     }
 
-    function balance(uint256 _tokenId) public view returns(bool) {
+    function balance(uint256 _tokenId) public view returns (bool) {
         return oldToken.ownerOf(_tokenId) == msg.sender;
     }
 
     function swap(uint256[] memory _tokenIds) public {
-    
         // Transfer old tokens to contract
         for (uint i = 0; i < _tokenIds.length; i++) {
             oldToken.transferFrom(msg.sender, address(this), _tokenIds[i]);
         }
-    
+
         uint256 numNewTokens = newToken.balanceOf(address(this));
-        require(numNewTokens >= _tokenIds.length, "Not enough new tokens in contract");
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            uint256 newTokenId = IERC721Enumerable(address(newToken)).tokenOfOwnerByIndex(address(this), i);
+        require(
+            numNewTokens >= _tokenIds.length,
+            "Not enough new tokens in contract"
+        );
+        for (uint256 i = _tokenIds.length - 1; i >= 0; i--) {
+            uint256 newTokenId = IERC721Enumerable(address(newToken))
+                .tokenOfOwnerByIndex(address(this), i);
             newToken.transferFrom(address(this), msg.sender, newTokenId);
         }
     }
 
     function withdrawOldTokens(uint256[] memory _tokenIds) public {
-        require(msg.sender == owner, "Only the contract owner can withdraw old tokens.");
+        require(
+            msg.sender == owner,
+            "Only the contract owner can withdraw old tokens."
+        );
         for (uint i = 0; i < _tokenIds.length; i++) {
             oldToken.transferFrom(address(this), msg.sender, _tokenIds[i]);
         }
     }
 
     function withdrawNewTokens(uint256[] memory _tokenIds) public {
-        require(msg.sender == owner, "Only the contract owner can withdraw old tokens.");
+        require(
+            msg.sender == owner,
+            "Only the contract owner can withdraw old tokens."
+        );
         for (uint i = 0; i < _tokenIds.length; i++) {
             newToken.transferFrom(address(this), msg.sender, _tokenIds[i]);
         }
     }
 
-
     function depositNewTokens(uint256[] memory _tokenIds) public {
-        require(msg.sender == owner, "Only the contract owner can deposit new tokens.");
+        require(
+            msg.sender == owner,
+            "Only the contract owner can deposit new tokens."
+        );
         for (uint i = 0; i < _tokenIds.length; i++) {
             newToken.transferFrom(msg.sender, address(this), _tokenIds[i]);
         }
